4c364e31ee625122872462ba764c5e38c3aa32df
Min Woo <minwoo@iMac5.local>
2018-10-29 15:12:21 -0400

    finished writing encoding part

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,7 +1,27 @@
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-word = "apple"
+phrase = "Python is fun"
 
-for x in word:
-    print(alphabet.index(x))
+phrase = phrase.lower()
+
+phrase = phrase.replace(" ", "")
+
+print(phrase)
+
+key = "apple"
+
+key_count = 0
+
+code = ""
+
+for x in phrase:
+    thing = alphabet.index(x) + alphabet.index(key[key_count % len(key)])
+    print(thing)
+    if thing >= 26:
+        code = code + alphabet[thing % 26]
+    else:
+        code = code + alphabet[thing]
+    key_count = key_count + 1
+
+print(code)
